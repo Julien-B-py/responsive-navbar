500b8cbc346d055ab516b438620e9e89e3d09efa
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2022-06-14 10:27:34 +0200

    v0.1
    Add SVG transition.

--- app.js
+++ app.js
@@ -1,39 +1,60 @@
+// Get svg and g elements
+const svg = document.getElementsByTagName("svg")[0];
+const g = svg.getElementsByTagName("g")[0];
+
+// Get ul element
 const ul = document.getElementsByTagName("ul")[0];
 
-const svg = document.getElementsByTagName("svg")[0];
+// Boolean checking if the min view port width matches 1200px
+var mediaQuery = window.matchMedia('(min-width: 1200px)');
 
-const svgPath = document.getElementsByTagName("path")[1];
-
-const mediaQuery = window.matchMedia('(min-width: 1200px)');
-
+// Boolean checking if the nav list is expanded
 var navExpanded = false;
 
+// When user clicks on the nav icon
 svg.onclick = () => {
 
+    // Toggle navExpanded boolean
     navExpanded = !navExpanded;
+    // Toggle SVG animation
+    g.classList.toggle("nc-int-icon-state-b")
 
+    // If nav list is expanded
     if (navExpanded) {
-        svgPath.setAttribute("d", "M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z");
+        // Display <ul>
         ul.style.display = "flex";
         return;
     }
 
-    svgPath.setAttribute("d", "M3 4h18v2H3V4zm6 7h12v2H9v-2zm-6 7h18v2H3v-2z");
+    // Hide <ul>
     ul.style.display = "none";
+
 }
 
+// When window is resized
 window.onresize = () => {
 
+    // Bool : <ul> has display flex ?
+    const isFlexUl = ul.style.display === "flex";
+
+    // If window width < 1200 px and nav list is expanded
     if (!mediaQuery.matches && navExpanded) {
+        // Display <ul>
         ul.style.display = "flex";
         return;
     }
 
+    // If window width < 1200 px and nav list is not expanded
     if (!mediaQuery.matches && !navExpanded) {
+        // Hide <ul>
         ul.style.display = "none";
         return;
     }
 
-    ul.style.display = "flex";
+    // If window width >= 1200 px and <ul> is not displayed in flex
+    if (!isFlexUl) {
+        // Display <ul>
+        ul.style.display = "flex";
+    }
 
 }
